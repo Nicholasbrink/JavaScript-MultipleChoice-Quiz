c9ea4a2db13ec25d8f98fe95ade08569cf74ba5d
Nicholas Brink <nicbrink@gmail.com>
2023-01-15 21:14:37 +0000

    added timer and question choices code

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,72 +1,106 @@
 //add start button
 
-// add all variables (selectors) to point to the HTML
-//rulesBox;
+// Add all variables (selectors) to link to the HTML
+var questions = document.querySelector("#questions");
+var timer = document.querySelector("#time");
+var choices = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startButton = document.querySelector("#start");
+var initials = document.querySelector("#initials");
+var feedback = document.querySelector("#feedback");
 
-//questionsTitle
-// choicesContainer
-//timerContainer
-//...
-let startButton = document.getElementById("start");
-var currentQuestion = 0;
+//add global variables
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerID;
+
+// var secondsLeft = 90;
+// var score = 0;
+// var allScores = [];
+
+//add localStorage scores
+
+//Set the Quiz variables
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerID;
 
 //start Quiz
+function startQuiz() {
+  var startScreen = document.getElementById("start-screen");
+  startScreen.setAttribute("class", "hide"); // hide start screen
 
-startButton.oneclick = () => {};
-function populateQuestion(question) {
-  var question = question.title;
-  var choices = question.choices;
-  var answer = question.answer;
+  // show questions
+  questions.removeAttribute("class"); // show question section
 
-  // questionTitle.textContent = question;
-  var choicesList = document.createElement("ul");
-  for (let i = 0; i < choices.length; i++) {
-    var choice = document.createElement("li");
-    choice.textContent = choices[i];
-    choicesList.appendChild(choice);
-  }
-  // choicesContainer.appendChild(choicesList)
-
-  function endGame() {}
-  //when the game ends, it should display their score and give the user the ability to save their initials and their score
-  // hide questions container
-  //show endscreen continer
-  //assign score to finalScore CSSContainerRule
-  //reset the timer
-  //
-
-  function nextQuestion() {
-    currentQuestion++;
-    //check if we are on the last question
-    if (currentQuestion < questions.length) {
-      populateQuestion(questions[currentQuestion]);
-    } else {
-      endGame();
-    }
-  }
-
-  var counter;
-
-  //select start button
-  startButton.addEventListener("click", function () {
-    startScreenElement.setAttribute("class", "hide");
-    questionsContainer.setAttribute("class", "visible");
-
-    //show the first question
-    currentQuestion = 0;
-    populateQuestion(questions[currentQuestion]);
-
-    counter = 100;
-    timer = setInterval(function () {
-      counter--;
-      //set timerContainer text to counter
-      if (counter <= o) {
-        //endGame()
-        clearInterval(timer);
-      }
-    }, 1000);
-  });
+  //start timer and show starting time
+  timerID = setInterval(clockTick, 1000);
+  timer.textContent = time;
+  getQuestion();
 }
+
+// Get current question from the questions Array
+function getQuestion() {
+  var currentQuestion = questions[currentQuestionIndex]; //Where does currentQuestionIndex arise?
+}
+// update title with current question
+var title = document.getElementById("question-title");
+title.textContent = currentQuestion.title;
+
+//This is to clear out any old question choices
+choices.innerHTML = "";
+
+// questionTitle.textContent = question;
+
+//Create title and possible answer choices and loop over choices
+
+//Loop over choices
+currentQuestion.choices.forEach(function (choice, i) {
+  // create new button for each choice
+  var choice = document.createElement("button");
+  choice.setAttribute("class", "choice");
+  choice.setAttribute("value", "choice");
+  choice.textContent = i + 1 + ". " + choice;
+  // evenListener ?
+  choice.onclick = questionClick;
+  //display choice on the page
+  choices.appendChild(choice);
+});
+
+// startButton.addEventListener("click", function () {
+//   startScreen.setAttribute("class", "hide");
+//   questions.setAttribute("class", "visible");
+
+//Next question
+currentQuestion++;
+
+function nextQuestion() {
+  //check if we are on the last question
+  if (currentQuestion < questions.length) {
+    populateQuestion(questions[currentQuestionIndex]);
+  } else {
+    endGame();
+  }
+}
+
+var counter;
+
+//show the first question
+var currentQuestion = 0;
+populateQuestion(questions[currentQuestion]);
+
+counter = 100;
+timer = setInterval(function () {
+  counter--;
+  //set timerContainer text to counter
+  if (counter <= o) {
+    //endGame()
+    clearInterval(timer);
+  }
+}, 1000);
+
+// show end screen
+var endscreen = document.getElementById("end-screen");
 
 function saveHighScore(initial) {
   // get the current highscore value from local storage
@@ -84,3 +118,23 @@ function saveHighScore(initial) {
 //      var initial = initialInput.value.trim()
 //      save highscore(initial) --> score to local storage - save as an array or object
 //      redirect to highscore page
+
+//old Code
+
+//  //select start button
+//  startButton.addEventListener("click", function () {
+//   startScreen.setAttribute("class", "hide");
+//   questions.setAttribute("class", "visible");
+
+// choicesContainer.appendChild(choicesList)
+
+// function endGame() {}
+//when the game ends, it should display their score and give the user the ability to save their initials and their score
+// hide questions container
+//show endscreen continer
+//assign score to finalScore CSSContainerRule
+//reset the timer
+//
+
+// start quiz
+startButton.onclick = startQuiz;
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,51 +1,56 @@
-let question = [
+let questions = [
   {
     title: "What does CSS stand for?", //title
-    answer: "Cascading Style Sheet",
     choices: [
       "Condensed Scanned Sheet",
       "cascading scanned Style",
       "Condensed Style Scanner",
+      "Cascading Style Sheet",
     ],
+    answer: "Cascading Style Sheet",
   },
 
   {
-    title: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    title: "What does CSS stand for?", //title
     choices: [
       "Condensed Scanned Sheet",
       "cascading scanned Style",
       "Condensed Style Scanner",
+      "Cascading Style Sheet",
     ],
+    answer: "Cascading Style Sheet",
   },
 
   {
-    title: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    title: "What does CSS stand for?", //title
     choices: [
       "Condensed Scanned Sheet",
       "cascading scanned Style",
       "Condensed Style Scanner",
+      "Cascading Style Sheet",
     ],
+    answer: "Cascading Style Sheet",
   },
 
   {
-    title: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    title: "What does CSS stand for?", //title
     choices: [
       "Condensed Scanned Sheet",
       "cascading scanned Style",
       "Condensed Style Scanner",
+      "Cascading Style Sheet",
     ],
+    answer: "Cascading Style Sheet",
   },
 
   {
-    title: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    title: "What does CSS stand for?", //title
     choices: [
       "Condensed Scanned Sheet",
       "cascading scanned Style",
       "Condensed Style Scanner",
+      "Cascading Style Sheet",
     ],
+    answer: "Cascading Style Sheet",
   },
 ];
